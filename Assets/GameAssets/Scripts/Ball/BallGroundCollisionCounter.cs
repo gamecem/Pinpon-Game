b3641f08827f8b727164e6247c324399753595e7
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-07-19 15:40:19 +0300

    feat: collision counter

--- Assets/GameAssets/Scripts/Ball/BallGroundCollisionCounter.cs
+++ Assets/GameAssets/Scripts/Ball/BallGroundCollisionCounter.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallGroundCollisionCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Consts
+    private const string Ground = nameof(Ground);
+    #endregion
+
+    #region Variables
+    private int groundCollisionCount;
+    #endregion
+    private void Start()
     {
-        
+        groundCollisionCount = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GroundCollisionCount { get => groundCollisionCount; set => groundCollisionCount = value; }
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag(Ground))
+        {
+            GroundCollisionCount++;
+            Debug.Log("Ground Collisions: " + GroundCollisionCount);
+        }
+    }
+
+    public int GetGroundCollisionCount()
+    {
+        return GroundCollisionCount;
+    }
+
+    public void ResetGroundCollisionCount()
+    {
+        GroundCollisionCount = 0;
     }
 }
