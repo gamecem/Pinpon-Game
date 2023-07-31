d2fb2aaeaa7dd52f67156a195a818a8a33d6e28a
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-07-31 23:38:47 +0300

    feat: Winbox win & lose

--- Assets/GameAssets/Scripts/Ball/BallGroundCollisionCounter.cs
+++ Assets/GameAssets/Scripts/Ball/BallGroundCollisionCounter.cs
@@ -1,13 +1,20 @@
+using GameAssets.Scripts.Game;
+using GameAssets.Scripts.Level;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GameAssets.Scripts.Ball
 {
     public class BallGroundCollisionCounter : MonoBehaviour
     {
+        [System.Serializable]
+        public class GroundCollisionEvent : UnityEvent<int> { }
+
         #region Consts
         private const string Ground = nameof(Ground);
+        private const string WinBox = nameof(WinBox);
         #endregion
 
         #region Reactive Variables
@@ -15,22 +22,50 @@ namespace GameAssets.Scripts.Ball
         public IReadOnlyReactiveProperty<int> GroundCollisionCount => groundCollisionCount;
         #endregion
 
+        [SerializeField] private GroundCollisionEvent onGroundCollision;
+
         private void Start()
         {
             groundCollisionCount.Value = 0;
+            CountGroundCollision();
+            HandleWinBoxCollision();
+        }
 
+        public void ResetGroundCollisionCount()
+        {
+            groundCollisionCount.Value = 0;
+        }
+
+        private void CountGroundCollision()
+        {
             this.OnCollisionEnterAsObservable()
                 .Where(collision => collision.gameObject.CompareTag(Ground))
                 .Subscribe(_ =>
                 {
                     groundCollisionCount.Value++;
                     Debug.Log("Ground Collisions: " + groundCollisionCount.Value);
+
+                    onGroundCollision?.Invoke(groundCollisionCount.Value);
                 })
                 .AddTo(this);
         }
-        public void ResetGroundCollisionCount()
+
+        private void HandleWinBoxCollision()
         {
-            groundCollisionCount.Value = 0;
+            this.OnCollisionEnterAsObservable()
+                .Where(collision => collision.gameObject.CompareTag(WinBox))
+                .Subscribe(_ =>
+                {
+                    Debug.Log("You hit WinBox");
+                    if (LevelHandler.Instance.MaxCollideCount == groundCollisionCount.Value)
+                    {
+                        GameManager.Instance.WinGame();
+                    }
+                    else
+                    {
+                        GameManager.Instance.GameOver();
+                    }
+                }).AddTo(gameObject);
         }
     }
 }
--- Assets/GameAssets/Scripts/Level/LevelHandler.cs
+++ Assets/GameAssets/Scripts/Level/LevelHandler.cs
@@ -1,23 +1,32 @@
-
 using UnityEngine;
 
 namespace GameAssets.Scripts.Level
 {
     public class LevelHandler : MonoBehaviour
     {
+        private static LevelHandler instance;
+        public static LevelHandler Instance => instance;
+
         [SerializeField] private Level level;
         private int maxCollideCount;
         private int ballInLevelCount;
+        public int MaxCollideCount => maxCollideCount;
+        public int BallInLevelCount => ballInLevelCount;
 
-        private void Start()
+        private void Awake()
         {
+            // Enforce the singleton pattern
+            if (instance != null && instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); 
+
             maxCollideCount = level.MaxCollideCount;
             ballInLevelCount = level.BallInLevelCount;
         }
-        
-        
-        
-        
-        
     }
 }
